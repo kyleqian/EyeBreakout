8720647ede91694d2f052626c65bf1bc09e53c84
Kyle Qian <kylecqian@gmail.com>
2018-04-21 20:45:20 -0700

    Basic black hole

---
+++ Assets/GravityBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GravityBall : MonoBehaviour
+{
+    public float forceMultiplier;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (BlackHole.Instance.active)
+        {
+            float distance = Vector3.Distance(BlackHole.Instance.transform.position, transform.position);
+            Vector3 direction = BlackHole.Instance.transform.position - transform.position;
+            rb.AddForce(Vector3.Normalize(direction) * forceMultiplier / distance);
+        }
+    }
+}
---
+++ Assets/_SCRIPTS/BlackHole.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    public static BlackHole Instance;
+    public bool active;
+
+    private MeshRenderer mr;
+    private Collider col;
+    private float depth;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        mr = GetComponent<MeshRenderer>();
+        col = GetComponent<Collider>();
+        depth = transform.position.z - Camera.main.transform.position.z;
+    }
+
+    private void Update()
+    {
+        active = Input.GetMouseButton(0);
+        mr.enabled = active;
+        col.enabled = active;
+
+        if (active)
+        {
+            var mousePos = Input.mousePosition;
+            mousePos.z = depth;
+            transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+    }
+}
